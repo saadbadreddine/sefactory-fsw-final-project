4a114c6e59085fe1b76b7e5261fa8c57032623fb
Saad <badreddinesaad91@gmail.com>
2022-03-13 22:01:48 +0200

    Added EditPost API and EditPostInput structure to allow users to update their posts

--- controllers/user_actions.go
+++ controllers/user_actions.go
@@ -46,3 +46,43 @@ func Post(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "Posted successfully!"})
 }
+
+type EditPostInput struct {
+	ID        uint   `json:"id" binding:"required"`
+	SportID   uint   `json:"sport_id,omitempty"`
+	Time      string `json:"time,omitempty"`
+	Message   string `json:"message"`
+	Latitude  string `json:"latitude,omitempty"`
+	Longitude string `json:"longitude,omitempty"`
+}
+
+func EditPost(c *gin.Context) {
+
+	var input EditPostInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user_id, err := token.ExtractTokenID(c)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	p := models.Post{}
+	p.ID = input.ID
+	p.UserID = user_id
+	p.SportID = input.SportID
+	p.Message = input.Message
+	p.Time = input.Time
+	p.Latitude = input.Latitude
+	p.Longitude = input.Longitude
+
+	if _, err := p.UpdatePost(); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Post updated!"})
+}
