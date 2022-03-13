024c81f44773db9a175e74c0388df91789798ecd
Saad <badreddinesaad91@gmail.com>
2022-03-13 22:22:41 +0200

    Added ExtractTokenUserType function

--- utils/token/token.go
+++ utils/token/token.go
@@ -80,3 +80,26 @@ func ExtractTokenID(c *gin.Context) (uint, error) {
 	}
 	return 0, nil
 }
+
+func ExtractTokenUserType(c *gin.Context) (uint, error) {
+
+	tokenString := ExtractToken(c)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		utype, err := strconv.ParseUint(fmt.Sprintf("%.0f", claims["user_type"]), 10, 32)
+		if err != nil {
+			return 0, err
+		}
+		return uint(utype), nil
+	}
+	return 0, nil
+}
