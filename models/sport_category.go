9b6e7953eb7648c792813c67b927f3461fe2aafe
Saad <badreddinesaad91@gmail.com>
2022-03-13 22:14:39 +0200

    Added SaveSport function to allow the creation of a new sport category

--- models/sport_category.go
+++ models/sport_category.go
@@ -10,3 +10,14 @@ type SportCategory struct {
 	gorm.Model
 	Sport string `json:"sport" gorm:"size:not null"`
 }
+
+func (s *SportCategory) SaveSport() (*SportCategory, error) {
+
+	var err error = DB.Debug().Create(&s).Error
+
+	if err != nil {
+		return &SportCategory{}, err
+	}
+
+	return s, nil
+}
