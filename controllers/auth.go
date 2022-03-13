df41e7c4d00a976b920c419c29c88a253c78f3fc
Saad <badreddinesaad91@gmail.com>
2022-03-13 19:06:39 +0200

    Added Register API

--- controllers/auth.go
+++ controllers/auth.go
@@ -42,3 +42,37 @@ func Login(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"token": token})
 }
+
+type RegisterInput struct {
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+	Email     string `json:"email" binding:"required,email"`
+	Password  string `json:"password" binding:"required,max=16,min=6"`
+	Age       uint8  `json:"age"`
+	Gender    string `json:"gender" validate:"oneof=female male"`
+}
+
+func Register(c *gin.Context) {
+
+	var input RegisterInput
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	u := models.User{}
+	u.FirstName = input.FirstName
+	u.LastName = input.LastName
+	u.Email = input.Email
+	u.Password = input.Password
+
+	_, err := u.SaveUser()
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Registered successfully!"})
+}
