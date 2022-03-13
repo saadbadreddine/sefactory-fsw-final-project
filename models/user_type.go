e6912d37bca3679d3db415312a19af10536d1687
Saad <badreddinesaad91@gmail.com>
2022-03-13 22:33:27 +0200

    Added SaveUserType function for administration purposes to allow a superuser to create user types

--- models/user_type.go
+++ models/user_type.go
@@ -8,3 +8,14 @@ type UserType struct {
 	gorm.Model
 	Role string `json:"role" gorm:"size:not null "`
 }
+
+func (ut *UserType) SaveUserType() (*UserType, error) {
+
+	var err error = DB.Debug().Create(&ut).Error
+
+	if err != nil {
+		return &UserType{}, err
+	}
+
+	return ut, nil
+}
