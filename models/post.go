7a94304b8df44270c36f683169d4069ead97430d
Saad <badreddinesaad91@gmail.com>
2022-03-13 22:05:24 +0200

    Added UpdatePost function

--- models/post.go
+++ models/post.go
@@ -28,3 +28,29 @@ func (p *Post) SavePost() (*Post, error) {
 
 	return p, nil
 }
+
+func (p *Post) UpdatePost() (*Post, error) {
+
+	result := DB.Model(&Post{}).
+		Where("id = ?", p.ID).
+		Where("user_id = ?", p.UserID).
+		First(&p)
+
+	if result.Error == gorm.ErrRecordNotFound {
+		return &Post{}, errors.New("invalid request")
+	} else if result.Error != nil {
+		return &Post{}, result.Error
+	}
+
+	err := DB.Model(&Post{}).
+		Where("id = ?", p.ID).
+		Where("user_id = ?", p.UserID).
+		Omit("user_id, created_at").
+		Save(&p).Error
+
+	if err != nil {
+		return &Post{}, err
+	}
+
+	return p, nil
+}
