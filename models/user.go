adb18c0257a3bf959b7b459546bd551cb8590a10
Saad <badreddinesaad91@gmail.com>
2022-03-13 18:30:20 +0200

    Added LoginCheck and VerifyPassword functions to allow user login

--- models/user.go
+++ models/user.go
@@ -50,3 +50,32 @@ func (u *User) BeforeSave(*gorm.DB) error {
 
 	return nil
 }
+
+func VerifyPassword(password, hashedPassword string) error {
+
+	return bcrypt.
+		CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func LoginCheck(email string, password string) (string, error) {
+
+	var err error
+	u := User{}
+
+	err = DB.Model(User{}).Where("email = ?", email).Take(&u).Error
+	if err != nil {
+		return "Email not registered", err
+	}
+
+	err = VerifyPassword(password, u.Password)
+	if err != nil && err == bcrypt.ErrMismatchedHashAndPassword {
+		return "", err
+	}
+
+	token, err := token.GenerateToken(uint(u.ID), uint(u.UserTypeID))
+	if err != nil {
+		return "", err
+	}
+
+	return token, nil
+}
