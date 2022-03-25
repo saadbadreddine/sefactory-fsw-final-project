5dbb0fac83d45a2490a700681a8c277e42d1a285
Saad <badreddinesaad91@gmail.com>
2022-03-25 20:02:56 +0200

    Added loadEnv function to load the .env file before reading and mergeEnv function to load and write the jwt token and firebase uuid to the .env file

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -1,3 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 const storage = FlutterSecureStorage();
+
+loadEnv() async {
+  await dotenv.load(fileName: "assets/.env");
+}
+
+mergeEnv(String jwtToken, String firebaseToken) async {
+  await dotenv.load(
+      fileName: "assets/.env",
+      mergeWith: {'JWT_TOKEN': jwtToken, 'FIREBASE_TOKEN': firebaseToken});
+}
