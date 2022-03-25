8b491bedd3a0897384c04a8d638d326e03f42f49
Saad <badreddinesaad91@gmail.com>
2022-03-25 20:00:37 +0200

    Map widget's theme is now dynamic

--- lib/widget/map_widget.dart
+++ lib/widget/map_widget.dart
@@ -1,31 +1,70 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapWidget extends StatefulWidget {
-  const MapWidget({Key? key}) : super(key: key);
+class Map extends StatefulWidget {
+  const Map({Key? key}) : super(key: key);
 
   @override
-  _MapWidgetState createState() => _MapWidgetState();
+  State<StatefulWidget> createState() => _MapState();
 }
 
-class _MapWidgetState extends State<MapWidget> {
-  late GoogleMapController mapController;
+class _MapState extends State<Map> with WidgetsBindingObserver {
+  final Completer<GoogleMapController> _controller = Completer();
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  String? _darkMapStyle;
+  String? _lightMapStyle;
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _loadMapStyles();
+  }
+
+  Future _loadMapStyles() async {
+    _darkMapStyle = await rootBundle.loadString('assets/map_styles/dark.json');
+    _lightMapStyle =
+        await rootBundle.loadString('assets/map_styles/light.json');
+  }
+
+  Future _setMapStyle() async {
+    final controller = await _controller.future;
+    final theme = WidgetsBinding.instance.window.platformBrightness;
+    if (theme == Brightness.dark)
+      controller.setMapStyle(_darkMapStyle);
+    else
+      controller.setMapStyle(_lightMapStyle);
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    setState(() {
+      _setMapStyle();
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    CameraPosition initialLocation =
+        const CameraPosition(zoom: 11, target: LatLng(45.521563, -122.677433));
+
     return GoogleMap(
-      onMapCreated: _onMapCreated,
-      zoomControlsEnabled: false,
-      initialCameraPosition: CameraPosition(
-        target: _center,
-        zoom: 11.0,
-      ),
-    );
+        mapType: MapType.normal,
+        myLocationEnabled: true,
+        zoomControlsEnabled: false,
+        initialCameraPosition: initialLocation,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+          _setMapStyle();
+        });
   }
 }
