6f70e7b1d1833602b0b8ee79b7e373b4218d8cfe
Saad <badreddinesaad91@gmail.com>
2022-03-29 19:30:35 +0300

    Added SaveConnection, FindAllConnections, UpdateConnection, and DeleteConnection functions in Connection model

--- models/connection.go
+++ models/connection.go
@@ -14,3 +14,58 @@ type Connection struct {
 	Receiver   User `json:"-" gorm:"foreignKey:ReceiverID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	IsAccepted bool `json:"is_accepted" gorm:"size:not null;default:false"`
 }
+
+func (c *Connection) SaveConnection() (*Connection, error) {
+
+	var err error = DB.Debug().Create(&c).Error
+
+	if err != nil {
+		return &Connection{}, err
+	}
+
+	return c, nil
+}
+
+func FindAllConnections() ([]Connection, error) {
+
+	var err error
+	connections := []Connection{}
+	err = DB.Debug().Model(&Connection{}).Limit(100).Find(&connections).Error
+	if err != nil {
+		return connections, err
+	}
+	return connections, err
+}
+
+func GetConnectionByID(con_id uint) (Connection, error) {
+
+	c := Connection{}
+
+	if err := DB.First(&c, con_id).Error; err != nil {
+		return c, errors.New("not found")
+	}
+
+	return c, nil
+}
+
+func (c *Connection) UpdateConnection() (*Connection, error) {
+
+	var err error = DB.Debug().Save(&c).Error
+
+	if err != nil {
+		return &Connection{}, err
+	}
+
+	return c, nil
+}
+
+func (c *Connection) DeleteConnection() (*Connection, error) {
+
+	var err error = DB.Debug().Delete(&c).Error
+
+	if err != nil {
+		return &Connection{}, err
+	}
+
+	return c, nil
+}
