1305742599841b358457991d59f8b90f75768961
Saad <badreddinesaad91@gmail.com>
2022-04-02 20:05:41 +0300

    Cleaning

--- frontend/lib/model/login_model.dart
+++ frontend/lib/model/login_model.dart
@@ -19,16 +19,16 @@ class LoginRequest {
 
 class LoginResponse {
   final String token;
-  final String firebaseToken;
+  final String firebaseID;
   final String error;
 
   LoginResponse(
-      {required this.token, required this.firebaseToken, required this.error});
+      {required this.token, required this.firebaseID, required this.error});
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) {
     return LoginResponse(
         token: json["token"] ?? "",
-        firebaseToken: json["firebase_token"] ?? "",
+        firebaseID: json["firebase_id"] ?? "",
         error: json["error"] ?? "");
   }
 }
