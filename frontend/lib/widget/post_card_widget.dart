568ffa796e2268fbcdfc84bccb547dbc8f8f331e
Saad <badreddinesaad91@gmail.com>
2022-04-03 03:27:47 +0300

    Send request now adds a document in firebase's requests collection. Sending a request disables the button, send request button is also disable if a request already exists for that user

--- frontend/lib/widget/post_card_widget.dart
+++ frontend/lib/widget/post_card_widget.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hustle_app/api/post_service.dart';
 import 'package:hustle_app/model/post_model.dart';
@@ -14,6 +15,8 @@ class PostCard extends StatefulWidget {
   final String imageURL;
   final int postID;
   final VoidCallback onDeletedPost;
+  final VoidCallback onRequestSent;
+  final bool isOnMap;
 
   const PostCard({
     Key? key,
@@ -27,6 +30,8 @@ class PostCard extends StatefulWidget {
     required this.imageURL,
     required this.postID,
     required this.onDeletedPost,
+    required this.onRequestSent,
+    required this.isOnMap,
   }) : super(key: key);
 
   @override
@@ -38,6 +43,7 @@ class _PostCardState extends State<PostCard> {
   late DeletePostRequest deletePostRequest;
   late DeletePostResponse deletePostresponse;
   late PostService postService = PostService();
+  bool _isRequested = true;
 
   @override
   void initState() {
@@ -45,189 +51,231 @@ class _PostCardState extends State<PostCard> {
     if (widget.firebaseID == firebaseID) {
       _isMine = true;
     }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      checkrequestExists();
+    });
   }
 
   @override
   Widget build(context) {
-    return Card(
-      semanticContainer: true,
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      elevation: 5,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      child: Container(
-        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: 55,
-                  height: 55,
-                  child: ClipOval(
-                    child: Material(
-                      color: Theme.of(context).colorScheme.onBackground,
-                      child: Image.network(
-                        widget.imageURL.toString(),
-                        height: 150.0,
-                        width: 100.0,
-                        fit: BoxFit.fill,
+    return Wrap(children: <Widget>[
+      Card(
+        semanticContainer: true,
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        margin: widget.isOnMap
+            ? const EdgeInsets.symmetric(horizontal: 20, vertical: 200)
+            : const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Container(
+          padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 55,
+                    height: 55,
+                    child: ClipOval(
+                      child: Material(
+                        color: Theme.of(context).colorScheme.onBackground,
+                        child: Image.network(
+                          widget.imageURL.toString(),
+                          height: 150.0,
+                          width: 100.0,
+                          fit: BoxFit.fill,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(top: 15),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 15),
-                              child: Text(
-                                '${widget.firstName} ${widget.lastName}',
-                                style: const TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w500),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            const SizedBox(height: 10),
-            Text(
-              'Sport:  ${widget.sport}',
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              'Time: ${widget.time}',
-              style: const TextStyle(
-                fontSize: 15,
-                height: 1.4,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 15),
-            Align(
-                child: Text(
-                  widget.message,
-                  style: const TextStyle(fontSize: 16, height: 1.4),
-                ),
-                alignment: Alignment.topLeft),
-            const SizedBox(height: 10),
-            Container(
-                child: !_isMine
-                    ? Align(
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 6.0),
-                          child: TextButton(
-                            onPressed: () => {},
-                            child: const Text('Send Request',
-                                style: TextStyle(fontSize: 15)),
-                          ),
-                        ),
-                        alignment: Alignment.bottomRight,
-                      )
-                    : ButtonBar(
-                        children: [
-                          TextButton(
-                            onPressed: () => {
-                              showDialog<String>(
-                                context: context,
-                                builder: (BuildContext context) => SizedBox(
-                                  width: 300,
-                                  height: 400,
-                                  child: AlertDialog(
-                                    title: const Text('Alert'),
-                                    content: const Text(
-                                        'Are you sure you want to delete your post?'),
-                                    actions: <Widget>[
-                                      TextButton(
-                                        onPressed: () =>
-                                            Navigator.pop(context, 'Cancel'),
-                                        child: const Text('Cancel'),
-                                      ),
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.pop(context, 'OK');
-                                          deletePostRequest = DeletePostRequest(
-                                              postID: widget.postID);
-                                          postService
-                                              .deletePost(
-                                                  deletePostRequest, jwtToken!)
-                                              .then((value) {
-                                            if (!value.error.isNotEmpty) {
-                                              widget.onDeletedPost();
-                                            }
-                                          });
-                                        },
-                                        child: Text(
-                                          'OK',
-                                          style: TextStyle(
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .error,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(top: 15),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 15),
+                                child: Text(
+                                  '${widget.firstName} ${widget.lastName}',
+                                  style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500),
                                 ),
                               ),
-                            },
-                            child: Text('Delete',
-                                style: TextStyle(
-                                    color: Theme.of(context).colorScheme.error,
-                                    fontSize: 15)),
+                            ],
                           ),
-                        ],
-                      ))
-          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Sport:  ${widget.sport}',
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                'Time: ${widget.time}',
+                style: const TextStyle(
+                  fontSize: 15,
+                  height: 1.4,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 15),
+              Align(
+                  child: Text(
+                    widget.message,
+                    style: const TextStyle(fontSize: 16, height: 1.4),
+                  ),
+                  alignment: Alignment.topLeft),
+              const SizedBox(height: 10),
+              Container(
+                  child: !_isMine
+                      ? Align(
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 6.0),
+                            child: widget.isOnMap
+                                ? ButtonBar(children: [
+                                    TextButton(
+                                      onPressed: !_isRequested
+                                          ? () {
+                                              createDocument();
+                                              _isRequested = true;
+                                              setState(() {});
+                                            }
+                                          : null,
+                                      child: const Text('Send Request',
+                                          style: TextStyle(fontSize: 15)),
+                                    ),
+                                    TextButton(
+                                      onPressed: () =>
+                                          Navigator.pop(context, 'Dismiss'),
+                                      child: const Text('Dismiss'),
+                                    ),
+                                  ])
+                                : TextButton(
+                                    onPressed: !_isRequested
+                                        ? () {
+                                            createDocument();
+                                            _isRequested = true;
+                                            setState(() {});
+                                            widget.onRequestSent();
+                                          }
+                                        : null,
+                                    child: const Text('Send Request',
+                                        style: TextStyle(fontSize: 15)),
+                                  ),
+                          ),
+                          alignment: Alignment.bottomRight,
+                        )
+                      : ButtonBar(
+                          children: [
+                            TextButton(
+                              onPressed: () => {
+                                showDialog<String>(
+                                  context: context,
+                                  builder: (BuildContext context) => SizedBox(
+                                    width: 300,
+                                    height: 400,
+                                    child: AlertDialog(
+                                      title: const Text('Alert'),
+                                      content: const Text(
+                                          'Are you sure you want to delete your post?'),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          onPressed: () =>
+                                              Navigator.pop(context, 'Cancel'),
+                                          child: const Text('Cancel'),
+                                        ),
+                                        TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context, 'OK');
+                                            deletePostRequest =
+                                                DeletePostRequest(
+                                                    postID: widget.postID);
+                                            postService
+                                                .deletePost(deletePostRequest,
+                                                    jwtToken!)
+                                                .then((value) {
+                                              if (!value.error.isNotEmpty) {
+                                                widget.onDeletedPost();
+                                              }
+                                            });
+                                          },
+                                          child: Text(
+                                            'OK',
+                                            style: TextStyle(
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .error,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              },
+                              child: Text('Delete',
+                                  style: TextStyle(
+                                      color:
+                                          Theme.of(context).colorScheme.error,
+                                      fontSize: 15)),
+                            ),
+                          ],
+                        ))
+            ],
+          ),
         ),
       ),
-    ); /*Card(
-      clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(24),
-      ),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16).copyWith(bottom: 0),
-            child: Text(
-              "${widget.firstName} ${widget.lastName}",
-              style: const TextStyle(fontSize: 16),
-            ),
-          ),
-          ButtonBar(
-            alignment: MainAxisAlignment.end,
-            children: [
-              ElevatedButton(
-                child: const Text('Send Request'),
-                onPressed: () {},
-              ),
-              ElevatedButton(
-                child: const Text('Dismiss'),
-                onPressed: () {},
-              )
-            ],
-          )
-        ],
-      ),
-    );*/
+    ]);
+  }
+
+  Future createDocument() async {
+    if (!_isRequested) {
+      final refMessage =
+          FirebaseFirestore.instance.collection('requests').doc();
+      await refMessage.set({
+        'senderID': firebaseID,
+        'receiverID': widget.firebaseID,
+        'isAccepted': false
+      });
+    }
+  }
+
+  checkrequestExists() async {
+    QuerySnapshot querySender = await FirebaseFirestore.instance
+        .collection('requests')
+        .where('senderID', isEqualTo: firebaseID)
+        .where('receiverID', isEqualTo: widget.firebaseID)
+        .get();
+
+    QuerySnapshot queryReceiver = await FirebaseFirestore.instance
+        .collection('requests')
+        .where('senderID', isEqualTo: widget.firebaseID)
+        .where('receiverID', isEqualTo: firebaseID)
+        .get();
+    if (this.mounted) {
+      setState(() {
+        querySender.docs.isEmpty && queryReceiver.docs.isEmpty
+            ? _isRequested = false
+            : _isRequested = true;
+      });
+      print(_isRequested);
+    }
   }
 }
