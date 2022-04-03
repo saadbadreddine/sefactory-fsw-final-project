5616635cb4c67db9ddb55d036b8702f301a0d24c
Saad <badreddinesaad91@gmail.com>
2022-04-03 23:41:06 +0300

    Added requests stream and sendNotification function to push notifications

--- frontend/lib/api/firestore_service.dart
+++ frontend/lib/api/firestore_service.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hustle_app/utils/storage.dart';
+import 'package:http/http.dart' as http;
 
 class DatabaseService {
   final String? uid;
@@ -11,4 +14,30 @@ class DatabaseService {
   Stream<QuerySnapshot> get requests {
     return requestsCollection.where('receiverID', isEqualTo: email).snapshots();
   }
+}
+
+sendNotification(receiverToken, firstName, lastName) async {
+  print(receiverToken);
+  await http.post(
+    Uri.parse('https://fcm.googleapis.com/fcm/send'),
+    headers: <String, String>{
+      'Content-Type': 'application/json',
+      'Authorization': 'key=$serverKey',
+    },
+    body: jsonEncode(
+      <String, dynamic>{
+        'notification': <String, dynamic>{
+          'body': '${firstName} ${lastName}',
+          'title': 'Request Received',
+        },
+        'priority': 'high',
+        'data': <String, dynamic>{
+          'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+          'id': '1',
+          'status': 'done'
+        },
+        'to': receiverToken
+      },
+    ),
+  );
 }
